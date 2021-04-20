ca84529bb39971d1fafdb27abbcb06918ed42daa
Michael van Tellingen <m.vantellingen@labdigital.nl>
2021-04-20 22:14:10 +0200

    Backwards compatibility fix

--- amplience/utils.go
+++ amplience/utils.go
@@ -7,8 +7,10 @@ import (
 
 func parseID(id string) (hub_id string, resource_id string) {
 	values := strings.SplitN(id, ":", 2)
-
-	return values[0], values[1]
+	if len(values) > 1 {
+		return values[0], values[1]
+	}
+	return "", values[0]
 }
 
 func createID(hub_id string, resource_id string) string {
